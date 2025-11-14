97ad9400968acf0c18fcb36c988dc8771aedd79f
jcurcillo <jcurcillo@student.cscc.edu>
2025-11-13 21:05:45 -0500

    set up ship to reset when colliding with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,9 @@ class AlienInvasion:
         self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
         self.laser_sound.set_volume(0.7)
 
+        self.impact_sound = pygame.mixer.Sound(self.settings.impact_sound)
+        self.impact_sound.set_volume(0.7)
+
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet
@@ -36,9 +39,33 @@ class AlienInvasion:
             #act on input
             self.ship.update()
             self.alien_fleet.update_fleet()
+            #check for interactions
+            self._check_collisions()
             #output result
             self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _check_collisions(self):
+        # check collisions for ship
+        if self.ship.check_collisions(self.alien_fleet.fleet):
+            self._reset_level()
+            # -1 life
+
+        # check collisions for aliens and bottom of screen
+        if self.alien_fleet.check_fleet_bottom():
+            self._reset_level()
+
+        # check collisions of projectiles and aliens
+        collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
+        if collisions:
+            self.impact_sound.play()
+            self.impact_sound.fadeout(300)
+
+    def _reset_level(self):
+        self.ship.arsenal.arsenal.empty()
+        self.alien_fleet.fleet.empty()
+        self.alien_fleet.create_fleet()
+
 
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))       
@@ -71,7 +98,7 @@ class AlienInvasion:
         elif event.key == pygame.K_SPACE:
             if self.ship.fire():
                 self.laser_sound.play()
-                self.laser_sound.fadeout(300  )
+                self.laser_sound.fadeout(300)
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
--- ship.py
+++ ship.py
@@ -17,11 +17,14 @@ class Ship:
         self.image = pygame.transform.scale(self.image, (self.settings.ship_w, self.settings.ship_h))
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self._center_ship()
         self.moving_right = False
         self.moving_left = False
-        self.x = float(self.rect.x)
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midbottom = self.boundaries.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
         #updating ship position
@@ -47,3 +50,9 @@ class Ship:
     def fire(self):
         #adds bullet to arsenal list
         return self.arsenal.fire_bullet()
+    
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
